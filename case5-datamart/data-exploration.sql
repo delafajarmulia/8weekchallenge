05e9e6532857e5b5d2ff78bfb5ae63ea8170e4a1
delafjr <dela.fjr08@gmail.com>
2024-03-17 12:31:06 +0700

    add translate

--- case5-datamart/data-exploration.sql
+++ case5-datamart/data-exploration.sql
@@ -1,10 +1,12 @@
 -- 1. What day of the week is used for each week_date value?
+-- Hari apa dalam seminggu yang digunakan untuk setiap nilai tanggal_minggu?
 SELECT DAYNAME(week_date) AS dayname
 FROM clean_weekly_sales
 GROUP BY DAYNAME(week_date);
 
 
 -- 2. What range of week numbers are missing from the dataset?
+-- Berapa rentang angka minggu yang hilang dari kumpulan data?
 WITH cte1 AS (
 	SELECT *, DENSE_RANK() OVER(ORDER BY week_number) AS weeknumb
     FROM clean_weekly_sales) 
@@ -13,6 +15,7 @@ SELECT 52-COUNT(DISTINCT weeknumb) AS missing_no_of_weeks
 
 
 -- 3. How many total transactions were there for each year in the dataset?
+-- Berapa total transaksi yang ada untuk setiap tahun dalam kumpulan data?
 SELECT year AS calendar_year, SUM(transactions) AS total_transactions
 FROM clean_weekly_sales
 GROUP BY year
@@ -20,6 +23,7 @@ ORDER BY year;
 
 
 --  4. What is the total sales for each region for each month?
+-- Berapa total penjualan tiap wilayah tiap bulannya?
 SELECT region, month_number, SUM(sales) AS total_sales
 FROM clean_weekly_sales
 GROUP BY region, month_number
@@ -27,12 +31,14 @@ ORDER BY region, month_number;
 
 
 -- 5. What is the total count of transactions for each platform?
+-- Berapa jumlah total transaksi untuk setiap platform?
 SELECT platform, SUM(transactions) AS total_count
 FROM clean_weekly_sales
 GROUP BY platform;
 
 
 -- 6. What is the percentage of sales for Retail vs Shopify for each month?
+-- Berapa persentase penjualan Retail vs Shopify setiap bulannya?
 WITH cte1 AS(
 SELECT year, month_number, SUM(sales) AS tsm
 FROM clean_weekly_sales
@@ -45,6 +51,7 @@ ORDER BY cs.month_number;
 
 
 -- 7. What is the percentage of sales by demographic for each year in the dataset?
+-- Berapa persentase penjualan berdasarkan demografi untuk setiap tahun dalam kumpulan data?
 WITH cte1 AS (
 SELECT year, SUM(sales) AS tsm
 FROM clean_weekly_sales
@@ -57,6 +64,7 @@ ORDER by cs.year, cs.demographic;
 
 
 -- 8. Which age_band and demographic values contribute the most to Retail sales?
+-- Kelompok_usia dan nilai demografi manakah yang paling berkontribusi terhadap penjualan Ritel?
 SET @a=(SELECT SUM(sales) FROM clean_weekly_sales);
 SELECT age_band, demographic, ROUND((SUM(sales)/@a)*100,0) AS percentage
 FROM clean_weekly_sales
@@ -66,6 +74,7 @@ DESC;
 
 
 -- 9. Can we use the avg_transaction column to find the average transaction size for each year for Retail vs Shopify? If not - how would you calculate it instead?
+-- Bisakah kita menggunakan kolom avg_transaction untuk menemukan ukuran transaksi rata-rata setiap tahun untuk Retail vs Shopify? Jika tidak - bagaimana Anda menghitungnya?
 SELECT year, platform, ROUND(AVG(avg_transaction),0) AS avg_transaction_row, SUM(sales) / SUM(transactions) AS avg_transaction_group
 FROM clean_weekly_sales
 GROUP BY year, platform
